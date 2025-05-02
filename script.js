8e16c405c7706a91ef4727a9c6589cf3a810425b
Yojhan <sryojhan@gmail.com>
2025-05-02 03:04:54 +0200

    implemented wheel logic

--- script.js
+++ script.js
@@ -1,3 +1,88 @@
 
+class Manager{
 
-const initialGridSize = 100;
+    init(){
+
+        this.left = new Wheel(document.querySelector('#left-wheel'));
+        this.right = new Wheel(document.querySelector('#right-wheel'));
+
+        this.selected = null;
+
+        this.initialiseEvents();
+    }
+
+    initialiseEvents(){
+
+        
+        addEventListener('mouseup', (data)=>{
+
+            if(this.selected !== null){
+
+                this.selected = null;
+            }
+
+        });
+
+        addEventListener('mousemove', (data)=>{
+
+            if(this.selected !== null){
+
+                this.selected.onDrag(data.movementX);
+            }
+
+        });
+    }
+
+
+    selectWheel(wheel){
+
+        this.selected = wheel;
+    }
+
+}
+
+
+
+let manager = new Manager();
+
+
+class Wheel{
+
+    constructor(element){
+
+        // this.currentRotation = Math.random() * 360;
+         this.currentRotation = 0;
+        this.element = element;
+
+
+        element.addEventListener('mousedown', ()=>{
+
+            manager.selectWheel(this);
+        });
+
+        this.updateVisuals();
+    }    
+
+
+    updateVisuals(){
+
+        
+        this.element.children[0].style.transform = `rotate(${this.currentRotation}deg)`;
+    }
+
+    onDrag(movementX){
+
+        this.currentRotation += movementX;
+
+        this.updateVisuals();
+    }
+}
+
+
+
+
+
+addEventListener("DOMContentLoaded", (event) => {
+
+    manager.init();
+});
